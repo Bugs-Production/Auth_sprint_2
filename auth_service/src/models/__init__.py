154ff40016654a9e49700c83e28dc361a5cc50e0
Natalia B <naffa.bob@gmail.com>
2024-09-14 21:42:26 +0300

    Добавлены новые модели, исправлена модель User + миграции

--- auth_service/src/models/__init__.py
+++ auth_service/src/models/__init__.py
@@ -3,4 +3,6 @@
 from .roles import Role
 from .login_history import LoginHistory
 from .refresh_token import RefreshToken
+from .choices import OAuthProvidersEnum
+from .oauth import OAuthAccount
 from .user import User
---
+++ auth_service/src/models/choices.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class OAuthProvidersEnum(Enum):
+    # Yandex = 'yandex'
+    Google = "google"
